8d79c3ece6dacc03a63ca5a67186f8b88ecd87c9
lukasrenner2 <118417051+lukasrenner2@users.noreply.github.com>
2023-06-15 09:12:08 -0800

    added sudos

--- install.sh
+++ install.sh
@@ -1,41 +1,42 @@
 #!/bin/sh
 
-dnf install sway
-dnf install waybar
-dnf install xfce4-terminal
-dnf install fuzzel
-dnf install blueman
-dnf install network-manager-applet
-dnf install trash-cli
-dnf install mako
+sudo dnf install sway
+sudo dnf install waybar
+sudo dnf install xfce4-terminal
+sudo dnf install fuzzel
+sudo dnf install blueman
+sudo dnf install network-manager-applet
+sudo dnf install trash-cli
+sudo dnf install mako
 
 
 #zsh
-dnf install zsh
+sudo dnf install zsh
 chsh -s $(which zsh)
 sh -c "$(wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"  #oh-my-zsh
 #create symlinks for zsh themeing and customization
+rm .zshrc
 ln -s ~/.config/oh-my-zsh/dracula.zsh-theme ~/.oh-my-zsh/themes/dracula.zsh-theme
 ln -s ~/.config/oh-my-zsh/lib ~/.oh-my-zsh/themes/lib
 ln -s ~/.config/oh-my-zsh/.zshrc ~/.zshrc
 
 #neovim
-dnf install neovim
+sudo dnf install neovim
 git clone --depth 1 https://github.com/wbthomason/packer.nvim\
     ~/.local/share/nvim/site/pack/packer/start/packer.nvim
 
 #dracula gtk theme
 wget https://github.com/dracula/gtk/archive/master.zip
-unzip master.zip -d /usr/share/themes
+sudo unzip master.zip -d /usr/share/themes
 gsettings set org.gnome.desktop.interface gtk-theme "gtk-master"
 gsettings set org.gnome.desktop.wm.preferences theme "gtk-master"
 rm master.zip
 
 #dracula icon theme
 wget https://github.com/dracula/gtk/files/5214870/Dracula.zip
-unzip Dracula.zip -d /usr/share/themes
+sudo unzip Dracula.zip -d /usr/share/themes
 gsettings set org.gnome.desktop.interface icon-theme "Dracula"
 rm Dracula.zip
 
-dnf install breeze-cursor-theme
+sudo dnf install breeze-cursor-theme
 gsettings set org.gnome.desktop.interface breeze-cursor-theme
