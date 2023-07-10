3025a52f803eec9f10dfadfbcb2e25dcbf611590
lukasvrenner <lukasvrenner@icloud.com>
2023-07-09 19:33:53 -0800

    fixed home-manager installer

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-nix-channel --add https://github.com/nix-community/home-manager/archive/master.tar.gz home-manager
+nix-channel --add https://github.com/nix-community/home-manager/archive/release-23.05.tar.gz home-manager
 
 nix-channel --update
 nix-shell '<home-manager>' -A install
