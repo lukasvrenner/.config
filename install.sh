24dd36041c3c6be74dfc6f09461f0c4ca4acac2c
lukasvrenner <lukasvrenner@icloud.com>
2023-09-17 08:44:31 -0800

    added packer.nvim to install

--- install.sh
+++ install.sh
@@ -1,5 +1,8 @@
 sudo dnf install sway
 sudo dnf install foot
-sudo dnf install waybar
+sudo dnf install swaybar
 sudo dnf install neovim
 
+# packer -- neovim plugin manager
+git clone --depth 1 https://github.com/wbthomason/packer.nvim\
+ ~/.local/share/nvim/site/pack/packer/start/packer.nvim
