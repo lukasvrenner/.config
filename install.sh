8274fd45e6ab6a69e61dba0e3d513b40295d2ada
lukasvrenner <lukasvrenner@icloud.com>
2023-10-14 18:58:34 -0800

    improved install

--- install.sh
+++ install.sh
@@ -1,8 +1,19 @@
-sudo dnf install sway
-sudo dnf install foot
-sudo dnf install swaybar
-sudo dnf install neovim
+# this is a complete setup post-install script
 
+sudo dnf install sway # window manager
+sudo dnf install foot # blazingly fast and lightweight wayland terminal emulator
+sudo dnf install swaybar # status bar for sway
+sudo dnf install i3-status # status line generator
+
+# neovim
+sudo dnf install neovim
 # packer -- neovim plugin manager
 git clone --depth 1 https://github.com/wbthomason/packer.nvim\
  ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+sudo dnf install ripgrep # used for live grep in telescope.nvim
+nvim +PackerInstall +q # install plugins and quit
+
+# web browser
+sudo dnf install firefox # should be pre-installed
+
+# rust 
