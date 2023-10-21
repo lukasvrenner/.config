cb8875445f884ecd9bfca16912f59b5198db90e7
lukasvrenner <lukasvrenner@icloud.com>
2023-10-21 15:06:44 -0800

    removed plugins file

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,5 +1,5 @@
 vim.g.mapleader = ' '
-require('plugins')
+-- require('plugins')
 require('keymaps')
 
 -- Options
@@ -17,6 +17,7 @@ vim.o.cursorline = true -- highlight current line
 vim.cmd[[autocmd FileType * set formatoptions-=ro]] -- disables autocommenting on new lines
 vim.o.updatetime = 250
 vim.o.ignorecase = true -- case insensitive search
+vim.o.syntax = false
 -- stops highlighting after search is complete
 vim.opt.hlsearch = false
 vim.opt.incsearch = true
@@ -24,3 +25,42 @@ vim.opt.incsearch = true
 vim.cmd[[colorscheme gruvbox]]
 vim.opt.swapfile = false
 vim.termguicolors = true
+
+-- git clone --depth 1 https://github.com/wbthomason/packer.nvim\
+-- ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+
+require('packer').startup(function(use)
+	use 'wbthomason/packer.nvim' -- plugin manager
+
+	use 'windwp/nvim-autopairs' -- closes opening brackets and quotes
+
+	use 'nvim-treesitter/nvim-treesitter' --  improves syntax highlighting
+
+	use 'neovim/nvim-lspconfig' -- adds lsp support
+	use "williamboman/mason.nvim" -- "package manager" for language servers
+	use 'williamboman/mason-lspconfig.nvim' -- improves support for mason lspconfig
+
+    use 'hrsh7th/cmp-nvim-lsp'
+    use 'hrsh7th/cmp-buffer'
+    use 'hrsh7th/cmp-path'
+    use 'hrsh7th/nvim-cmp'
+    use 'L3MON4D3/LuaSnip'
+    use 'saadparwaiz1/cmp_luasnip'
+
+	use 'numToStr/Comment.nvim' -- allows for commenting vs gc and gcc
+
+	use {"ellisonleao/gruvbox.nvim"} -- gruvbox theme
+
+	-- fuzzy finder
+	use {
+	'nvim-telescope/telescope.nvim', tag = '0.1.4',
+	requires = { {'nvim-lua/plenary.nvim'} }
+	}
+
+
+end)
+
+require('nvim-autopairs').setup()
+
+-- use gcc to comment in normal mode and gc in visual mode
+require('Comment').setup()
