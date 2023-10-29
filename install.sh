858abb613d4ad7a5ea11df497d0c3cb805edb2ae
lukasvrenner <lukasvrenner@icloud.com>
2023-10-29 10:52:08 -0800

    now only install packer if nvim successfully installed

--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/bash
 # this is a complete setup post-install script
 
 sudo dnf install sway # window manager
@@ -7,13 +8,17 @@ sudo dnf install i3-status # status line generator
 
 # neovim
 sudo dnf install neovim
-# packer -- neovim plugin manager
-git clone --depth 1 https://github.com/wbthomason/packer.nvim\
- ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+if  ls /usr/bin/nvim | grep -q nvim; 
+then
+    # packer -- neovim plugin manager
+    git clone --depth 1 https://github.com/wbthomason/packer.nvim\
+        ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+    nvim +PackerInstall +q # install plugins and quit
+fi
+
 sudo dnf install ripgrep # used for live grep in telescope.nvim
-nvim +PackerInstall +q # install plugins and quit
 
 # web browser
-sudo dnf install firefox # should be pre-installed
+sudo dnf install firefox
 
 # rust 
