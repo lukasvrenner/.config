e6076141a1fd733fa4a41bd6ec3d88da362fcd72
lukasvrenner <lukasvrenner@icloud.com>
2023-10-29 17:18:41 -0800

    now checks if programs are installed, and if so, skips that one

--- install.sh
+++ install.sh
@@ -1,24 +1,51 @@
 #!/usr/bin/bash
 # this is a complete setup post-install script
+# REQUIRES ROOT 
+# if you want a rootless install, use rootless_install.sh
 
-sudo dnf install sway # window manager
-sudo dnf install foot # blazingly fast and lightweight wayland terminal emulator
-sudo dnf install swaybar # status bar for sway
-sudo dnf install i3-status # status line generator
+install () {
+    echo "installing $1"
+    if ls /usr/bin/ | grep -q $1; # checks if installed to boost speed if it isn't
+    then echo "$1 is already installed"
+    else sudo dnf install $1
+    fi
+}
+
+# sometimes, the executable has a different name from the package
+alt_install () {
+    echo "installing $1"
+    if ls /usr/bin | grep -q $2;
+    then echo "$1 is already installed"
+    else sudo dnf install $1;
+    fi
+}
+
+# window manager
+install sway
+install swaybar # status bar for sway
+
+# super efficient status bar generator
+install i3status
+
+# blazingly fast and lightweight wayland terminal emulator
+install foot
 
 # neovim
-sudo dnf install neovim
-if  ls /usr/bin/nvim | grep -q nvim; 
+alt_install neovim nvim
+# if it is installed or successfully installs,
+# then we can install plugin dependencies
+if  ls /usr/bin/ | grep -q nvim; 
 then
     # packer -- neovim plugin manager
     git clone --depth 1 https://github.com/wbthomason/packer.nvim\
         ~/.local/share/nvim/site/pack/packer/start/packer.nvim
     nvim +PackerInstall +q # install plugins and quit
+    alt_install ripgrep rg # a grep-like program used by telescope.nvim for text search
 fi
 
-sudo dnf install ripgrep # used for live grep in telescope.nvim
-
 # web browser
-sudo dnf install firefox
+install firefox
 
 # rust 
+install cargo # package manager
+install rustc # compiler
