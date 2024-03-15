128ae23fc519ef09f11308774e442d15e2c7a0ee
lukasvrenner <lukasvrenner@icloud.com>
2024-03-15 10:27:14 -0800

    removed clamshell mode do to system issues it caused

--- sway/clamshell.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-# don't turn on laptopt screen when reloading config if lid is shut
-if grep -q open /proc/acpi/button/lid/LID/state; then
-    swaymsg output LVDS-1 enable
-else
-    swaymsg output LVDS-1 disable
-fi
