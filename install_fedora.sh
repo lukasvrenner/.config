64e850267bcc49cb5752af2e3dc64adc43704559
lukasvrenner <lukasvrenner@icloud.com>
2024-07-12 19:55:41 -0800

    Add clang to install

--- install_fedora.sh
+++ install_fedora.sh
@@ -3,67 +3,60 @@
 # *NOT* distro-agnostic -- requires RHEL-based distro 
 # if you want a rootless install, use rootless_install.sh
 
-install () {
-    echo "installing $1"
-    if ls /usr/bin/$1; # checks if installed to boost speed if it isn't
-    then echo "$1 is already installed"
-    else sudo dnf install $1
-    fi
-}
-
-# sometimes, the executable has a different name from the package
-alt_install () {
-    echo "installing $1"
-    if ls /usr/bin/$2;
-    then echo "$1 is already installed"
-    else sudo dnf install $1;
-    fi
-}
+# install () {
+#     echo "installing $1"
+#     if ls /usr/bin/$1; # checks if installed to boost speed if it isn't
+#     then echo "$1 is already installed"
+#     else sudo dnf install $1
+#     fi
+# }
+#
+# # sometimes, the executable has a different name from the package
+# alt_install () {
+#     echo "installing $1"
+#     if ls /usr/bin/$2;
+#     then echo "$1 is already installed"
+#     else sudo dnf install $1;
+#     fi
+# }
 
 # symlink bash configs
-path_to_script=dirname $0
+path=dirname $0
 
-ln -s $path_to_script/.bashrc ~/.bashrc
-ln -s $path_to_script/.bash_profile ~/.bash_profile
+ln -s $path/.bashrc ~/.bashrc
+ln -s $path/.bash_profile ~/.bash_profile
+
+install=()
 
 # window manager
-install sway
-install swaybar # status bar for sway
+install+='sway'
+install+='swaybar' # status bar for sway
 
 # efficient status bar generator
-install i3status
+install+='i3status'
 
 # application launch
-install wmenu 
+install+='wmenu' 
 
 # blazingly fast and lightweight wayland terminal emulator
-install foot
+install+='foot'
 
 # a heavily refactored vim fork
-alt_install neovim nvim
-# if neovim is installed or successfully installs,
-# then we can install plugin dependencies
-if  ls /usr/bin/nvim; 
-then
-    # paq-nvim -- neovim plugin manager
-    git clone --depth=1 https://github.com/savq/paq-nvim.git \
-        "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/pack/paqs/start/paq-nvim
+install+='neovim'
+git clone --depth=1 https://github.com/savq/paq-nvim.git \
+    "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/pack/paqs/start/paq-nvim
 
-    nvim +PaqInstall +q # install plugins and quit
+nvim +PaqInstall +q # install plugins and quit
 
-    alt_install ripgrep rg # a grep-like program used by telescope.nvim for text search
-fi
+install+=ripgrep
 
 # web browser
-install firefox
+install+='firefox'
 
 # development 
-install gh
+install+='gh'
 
 # note: will require user input to complete
-echo "installing Rust requires downloading and executing a script from the web"
-echo "this is a potential security risk"
-read -p "would you like to continue? (y/N)" confirm
 if [[ $confirm == [yY] || $confirm == [yY][eE][sS] ]]
 then
     echo "installing Rust"
@@ -73,5 +66,11 @@ else
     echo "Rust was not installed"
 
 # file managers
-install nnn # terminal
-install Thunar # GTK
+install+=nnn # terminal
+install+=Thunar # GTK
+
+install+=clang
+install+=clang-tools-extra
+install+=clang-analyzer
+
+sudo dnf install ${install[*]}
